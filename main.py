0da6f01e0f119e9cbe6fef40291a3bf793f9d29a
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2023-12-28 11:01:02 +0530

    Project finished

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ link = argv[1]              #link is the first argument passed to the script
 yt = YouTube(link)         
 
 print("Title: ", yt.title)
+
+yd = yt.streams.get_by_resolution('720p') #this will get the video with 720p resolution(since there are multiple steams of video)
+
+print("Downloading...")
+yd.download("F:\Automated Video Downloader\Downloaded Video")
